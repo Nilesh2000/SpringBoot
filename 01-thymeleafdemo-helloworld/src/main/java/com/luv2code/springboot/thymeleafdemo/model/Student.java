d756f0b4d9fe2586047ab2c4156017ce16f87023
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-09-25 12:12:59 +0530

    thymeleaf demo

---
+++ 01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,54 @@
+package com.luv2code.springboot.thymeleafdemo.model;
+
+import java.util.List;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteProgrammingLanguage;
+    private List<String> favoriteSystems;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteProgrammingLanguage() {
+        return favoriteProgrammingLanguage;
+    }
+
+    public void setFavoriteProgrammingLanguage(String favoriteProgrammingLanguage) {
+        this.favoriteProgrammingLanguage = favoriteProgrammingLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+}
